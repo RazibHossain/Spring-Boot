bd9d16978bf3cdeb34b18fd34903cee58221454d
RazibHossain <razib@divine-it.net>
2026-04-09 15:36:10 +0600

    cofig server security added

---
+++ Smart Logistics and Order Management System/config-server/src/main/java/com/razib/config_server/SecurityConfig.java
@@ -0,0 +1,29 @@
+package com.razib.config_server;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http
+                // Disable CSRF for simplicity (important for POST requests like /refresh)
+                .csrf(csrf -> csrf.disable())
+
+                // Secure all endpoints
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().authenticated()
+                )
+
+                // Enable Basic Authentication
+                .httpBasic(Customizer.withDefaults());
+
+        return http.build();
+    }
+}
