974ac663966caba51eb30c413566cdeea2b6b2f5
RazibHossain <razib@divine-it.net>
2026-04-12 18:15:09 +0600

    config update

--- Smart Logistics and Order Management System/orderservice/src/main/java/com/razib/orderservice/feignclient/PaymentClient.java
+++ Smart Logistics and Order Management System/orderservice/src/main/java/com/razib/orderservice/feignclient/PaymentClient.java
@@ -7,7 +7,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(name = "paymentservice", url = "http://localhost:9030",fallback = PaymentClientFallback.class)
+@FeignClient(name = "paymentservice",fallback = PaymentClientFallback.class)
 public interface PaymentClient {
 
     @PostMapping("/payments")
